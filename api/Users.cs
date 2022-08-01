17d322faae2ee443482d1094f65e7d723456cb64
dc-o-kawamura <osamu.kawamura.j5h@jpgr.denso.com>
2022-08-01 14:59:11 +0900

    UsersのGETに対応

--- api/Users.cs
+++ api/Users.cs
@@ -1,35 +1,100 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using MeetingInfoNotifierAdmin.Api.Entities;
+using User = MeetingInfoNotifierAdmin.Api.Entities.User;
+using MeetingInfoNotifierAdmin.Api.Queries;
 
 namespace MeetingInfoNotifierAdmin.Api
 {
     public static class Users
     {
-        [FunctionName("Users")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        #region ユーザー情報一覧を取得
+        /// <summary>
+        /// ユーザー情報一覧を取得する。
+        /// </summary>
+        /// <param name="req">HTTPリクエスト</param>
+        /// <param name="client">CosmosDBのドキュメントクライアント</param>
+        /// <param name="log">ロガー</param>
+        /// <returns>ユーザー情報一覧</returns>
+        [FunctionName("GetUsers")]
+        public static async Task<IActionResult> GetUsers(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Users")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "meeting-info-db",
+                collectionName: "Users",
+                ConnectionStringSetting = "CosmosDbConnectionString")
+                ]DocumentClient client,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            string message = string.Empty;
 
-            string name = req.Query["name"];
+            try
+            {
+                log.LogInformation("GET Users");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+                // クエリパラメータから検索条件パラメータを設定
+                UsersQueryParameter queryParameter = new UsersQueryParameter()
+                {
+                    Ids = req.Query["ids"],
+                    Name = req.Query["name"],
+                    EmailAddress = req.Query["emailAddress"],
+                    UserPrincipal = req.Query["userPrincipal"]
+                };
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                // Slackチャンネル情報を取得
+                message = JsonConvert.SerializeObject(await GetUsers(client, queryParameter, log));
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex);
+            }
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(message);
         }
+
+        /// <summary>
+        /// ユーザー情報一覧を取得する。
+        /// </summary>
+        /// <param name="client">CosmosDBのドキュメントクライアント</param>
+        /// <param name="queryParameter">抽出条件パラメータ</param>
+        /// <param name="log">ロガー</param>
+        /// <returns>ユーザー情報一覧</returns>
+        internal static async Task<IEnumerable<User>> GetUsers(
+                   DocumentClient client,
+                   UsersQueryParameter queryParameter,
+                   ILogger log
+                   )
+        {
+            // Get a JSON document from the container.
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("meeting-info-db", "Users");
+            IDocumentQuery<User> query = client.CreateDocumentQuery<User>(collectionUri, new FeedOptions { EnableCrossPartitionQuery = true, PopulateQueryMetrics = true })
+                .Where(queryParameter.GetWhereExpression())
+                .AsDocumentQuery();
+            log.LogInformation(query.ToString());
+
+            var documentItems = new List<User>();
+            while (query.HasMoreResults)
+            {
+                foreach (var documentItem in await query.ExecuteNextAsync<User>())
+                {
+                    documentItems.Add(documentItem);
+                }
+            }
+            return documentItems;
+        }
+        #endregion
     }
 }
---
+++ api/entities/User.cs
@@ -0,0 +1,43 @@
+using System;
+using Newtonsoft.Json;
+
+namespace MeetingInfoNotifierAdmin.Api.Entities
+{
+    /// <summary>
+    /// ユーザーを表す
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// 既定のコンストラクタ
+        /// </summary>
+        public User()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        #region プロパティ
+        /// <summary>
+        /// 一意とするID
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        /// <summary>
+        /// ユーザー名
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        /// <summary>
+        /// メールアドレス
+        /// </summary>
+        [JsonProperty("emailAddress")]
+        public string EmailAddress { get; set; }
+        /// <summary>
+        /// ユーザープリンシパル名
+        /// </summary>
+        [JsonProperty("userPrincipal")]
+        public string UserPrincipal { get; set; }
+
+        #endregion
+    }
+}
